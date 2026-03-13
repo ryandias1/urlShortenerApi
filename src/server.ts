0af1bb7a11bee64d72ae59ba8d819d7b021d4e1f
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-03-13 10:29:52 -0300

    server usa rotas e middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from "express"
 import "dotenv/config"
+import { userRoutes } from "./routes/UserRoutes.js"
+import { urlRoutes } from "./routes/UrlRoutes.js"
 
 const app = express()
 const port = process.env.PORT
 app.use(express.json())
+app.use("/user", userRoutes)
+app.use("/url", urlRoutes)
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
