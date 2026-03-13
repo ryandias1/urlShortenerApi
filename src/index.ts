07c281ac47a6459033271bf33cb925d842c82210
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-03-13 10:29:31 -0300

    instancias de controller com dependencias

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { UrlController } from "./controller/UrlController.js";
+import { UserController } from "./controller/UserController.js";
+import { UrlRepository } from "./repository/implementations/UrlRepository.js";
+import { UserRepository } from "./repository/implementations/UserRepository.js";
+import { UrlService } from "./service/UrlService.js";
+import { UserService } from "./service/UserService.js";
+
+const userRepository = new UserRepository()
+const userService = new UserService(userRepository)
+export const userController = new UserController(userService)
+
+const urlRepository = new UrlRepository()
+const urlService = new UrlService(urlRepository)
+export const urlController = new UrlController(urlService)
